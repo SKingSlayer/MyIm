d5864451353dfaa34e267236e262337f8d09398b
70642 <706422117@qq.com>
2020-12-18 19:06:11 +0800

    2020.12.17 1.0version

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
+     @Autowired
+	 RedisTemplate<String, String> redisTemplate;
+    @Test
+	public  void test(){
+		redisTemplate.opsForList().leftPush("1","2");
+		redisTemplate.opsForList().leftPush("1","3");
+		System.out.println(redisTemplate.opsForList().range("1",0,-1));
 	}
 
 }
